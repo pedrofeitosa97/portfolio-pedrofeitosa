569c43ab70f6f18c82bd060705e7221a5801c126
Pedro Henrique Feitosa <50965091+pedrofeitosa97@users.noreply.github.com>
2023-04-15 01:52:00 -0300

    feat: corrigidos informações pessoais

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Pedro Feitosa",
+  githubUser: "pedrofeitosa97",
+  linkedinUser: "pedrohenriquefeitosa",
+  whatsappNumber: "+5531992172975",
+  emailUser: "pedron1001@gmail.com",
+  instagramUser: "",
   facebookUser: "",
 };
